cbe7819be30fac51a121bf7f80bbf1f6a7fb0445
Johannes Skjærbæk <johannes.skjaerbaek@gmail.com>
2018-03-25 21:14:01 +0200

    Trying to let people see who they want as center of the network.

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/*', function (req, res) {
+app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
